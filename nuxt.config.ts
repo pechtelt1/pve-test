91485368d96e6573022fce549172a623244ceeb1
pechtelt1 <patrick@van-echtelt.nl>
2025-01-23 16:56:33 +0100

    work

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+    compatibilityDate: '2024-11-01',
+    devtools: {enabled: true},
+    build: {
+        babel: {
+            presets({isServer}) {
+                return [
+                    [
+                        require.resolve('@nuxt/babel-preset-app'),
+                        {
+                            targets: isServer
+                                ? {node: 'current'}
+                                : {browsers: ['defaults', 'ie 11']}, // Ondersteuning voor oudere browsers
+                        },
+                    ],
+                ];
+            },
+        },
+    },
 })
